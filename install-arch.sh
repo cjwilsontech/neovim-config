cc048dffa80f81dec41516b3e1bd05ec7c05d231
cjwilsontech <cj@wilsontech.dev>
2025-05-15 01:16:38 -0700

    NVM fix for arch install script.

--- install-arch.sh
+++ install-arch.sh
@@ -1,6 +1,8 @@
-sudo pacman -S clang ripgrep python python-pip python-pynvim shfmt wl-clipboard nvm
+sudo pacman -S clang ripgrep python python-pip python-pynvim shfmt wl-clipboard nvm eslint_d
 
-. /usr/share/nvm/init-nvm.sh
+echo 'export NVM_DIR=~/.nvm' >>~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >>~/.zshrc
+source ~/.zshrc
 nvm install stable
 
 ./install-npm.sh
