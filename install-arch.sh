f0ef61f4ff8f876a3d421919947b0461d07ffe6e
cjwilsontech <cj@wilsontech.dev>
2025-05-15 00:51:48 -0700

    Update install script to include nvm on Arch.

--- install-arch.sh
+++ install-arch.sh
@@ -1,3 +1,6 @@
-sudo pacman -S clang ripgrep python python-pip python-pynvim shfmt wl-clipboard
+sudo pacman -S clang ripgrep python python-pip python-pynvim shfmt wl-clipboard nvm
+
+. /usr/share/nvm/init-nvm.sh
+nvm install stable
 
 ./install-npm.sh
